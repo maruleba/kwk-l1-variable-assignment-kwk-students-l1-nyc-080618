5235cc5f7a00c32b8404eb3b8c689d7ff13e5c19
maruleba <folaa1234@gmail.com>
2018-08-06 18:20:54 +0000

    automatically backed up by learn

--- prac.rb
+++ prac.rb
@@ -1,5 +1,6 @@
 first_name = "Mercy"
 last_name = "Aruleba"
+age = 18
 # String Interpolation 
-puts "My first name is #{first_name} and my last name is #{last_name}"
+puts "My first name is #{first_name} and my last name is #{age}"
 
