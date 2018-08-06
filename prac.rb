6d078511154178bd62de210c725c0fb8afd2408e
maruleba <folaa1234@gmail.com>
2018-08-06 18:22:38 +0000

    automatically backed up by learn

--- prac.rb
+++ prac.rb
@@ -2,5 +2,5 @@ first_name = "Mercy"
 last_name = "Aruleba"
 age = 18
 # String Interpolation 
-puts "My first name is #{first_name} and my last name is #{age}"
+puts "My first name is #{first_name} and i am #{age.to_s}"
 
